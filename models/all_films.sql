da1138af495fdfe8c9df1090644e8d770041f83f
Daniel K. Malungu <daniel.malungu@cloudfactory.com>
2022-03-24 05:56:55 +0000

    my_first_dbt_project_join

--- models/all_films.sql
+++ models/all_films.sql
@@ -1,2 +1,2 @@
 SELECT film_id, title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update, special_features, fulltext
-	FROM public.film;
+	FROM {{source('movie_rentals', 'film')}}
--- models/film_actor.sql
+++ models/film_actor.sql
@@ -1,4 +1,6 @@
 select *
 from {{ref('all_film_actor')}}
 inner join {{ref('all_films')}}
-on {{ref('all_film_actor')}}.code = {{ref('all_films')}}
+on {{ref('all_film_actor')}}.film_id = {{ref('all_films')}}.film_id 
+inner join {{ref('all_actors')}}
+on {{ref('all_film_actor')}}.actor_id  = {{ref('all_actors')}}.actor_id
