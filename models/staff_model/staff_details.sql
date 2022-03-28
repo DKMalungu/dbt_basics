c13b478cc4fcdb53d19f2d0b5e02b159d9d695db
Daniel K. Malungu <daniel.malungu@cloudfactory.com>
2022-03-28 18:19:22 +0000

    bug_fix: minor bugs

--- models/staff_model/staff_details.sql
+++ models/staff_model/staff_details.sql
@@ -1,8 +1,9 @@
 {{config(allias='staff_details')}}
+
 select * from {{ref('all_staff')}}
-inner join {{ref{'all_address'}}}
-on {{ref{'all_address'}}}.address_id = {{ref('all_staff')}}.address_id
+inner join {{ref('all_address')}}
+on {{ref('all_address')}}.address_id = {{ref('all_staff')}}.address_id
 inner join {{ref('all_cities')}}
-on {{ref('all_cities')}}.city_id = {{ref{'all_address'}}}.city_id
+on {{ref('all_cities')}}.city_id = {{ref('all_address')}}.city_id
 inner join {{ref('all_country')}}
 on {{ref('all_country')}}.country_id = {{ref('all_cities')}}.country_id
--- models/store_model/store_details.sql
+++ models/store_model/store_details.sql
@@ -1,10 +1,12 @@
 {{config(alias='store_details')}}
-select * from {{ref('all_stores')}}
+with store_details as (select * from {{ref('all_stores')}}
 inner join {{ref('all_staff')}}
 on {{ref('all_staff')}}.staff_id = {{ref('all_stores')}}.manager_staff_id
 inner join {{ref('all_address')}}
 on {{ref('all_address')}}.address_id = {{ref('all_stores')}}.address_id
 inner join {{ref('all_cities')}}
-on {{ref('all_cities')}}.city_id = {{ref{'all_address'}}}.city_id
+on {{ref('all_cities')}}.city_id = {{ref('all_address')}}.city_id
 inner join {{ref('all_country')}}
-on {{ref('all_country')}}.country_id = {{ref('all_cities')}}.country_id
+on {{ref('all_country')}}.country_id = {{ref('all_cities')}}.country_id)
+select first_name, last_name, email, address, address2, district, postal_code, phone, city, country
+from store_details
